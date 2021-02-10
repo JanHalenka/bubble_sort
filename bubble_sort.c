424dd51846df4d3c2e42e40644d5d80c7f0043ee
Jan Halenka <halenka.jan@gmail.com>
2021-02-10 22:35:31 +0100

    Creating an array

--- bubble_sort.c
+++ bubble_sort.c
@@ -4,6 +4,9 @@
 
 int main(void)
 {
+  // Creating an array
+  int unsrtd_nmbrs[] = {7, 5, 2, 8, 6, 3, 4, 1};
+  const int NUMBER = 8
   // Repeat n-1 times
     // For i from 0 to n-2
       // If i'th and i+1'th elements out of order
