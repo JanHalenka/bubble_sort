d8fee682de0d287a063ed24e8df7f22e97407cef
Jan Halenka <halenka.jan@gmail.com>
2021-02-10 22:39:18 +0100

    Loops added

--- bubble_sort.c
+++ bubble_sort.c
@@ -7,10 +7,18 @@ int main(void)
   // Creating an array
   int unsrtd_nmbrs[] = {7, 5, 2, 8, 6, 3, 4, 1};
   const int NUMBER = 8
+
   // Repeat n-1 times
+  for (int i = 0; i < NUMBER - 1; i++)
+  {
+    int counter = 0;
     // For i from 0 to n-2
+    for (int j = 0; j < NUMBER - 1; j++)
+    {
       // If i'th and i+1'th elements out of order
         // Swap them
+    }
     // If no swaps
       // Quit
+  }
 }
