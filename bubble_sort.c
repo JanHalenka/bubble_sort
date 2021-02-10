aa2915b641bef4ebaa1e21ba3839a9ac7d2dce28
Jan Halenka <halenka.jan@gmail.com>
2021-02-10 22:27:42 +0100

    CS50 pseudocode added

---
+++ bubble_sort.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Bubble Sort
+
+int main(void)
+{
+  // Repeat n-1 times
+    // For i from 0 to n-2
+      // If i'th and i+1'th elements out of order
+        // Swap them
+    // If no swaps
+      // Quit
+}
